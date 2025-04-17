9f9624bb4481cbe0d24809b45d9d554ae19a1cd5
danilo2420 <danimoraga1997@gmail.com>
2025-04-17 18:35:05 +0200

    users: get endpoints done

--- back/app.py
+++ back/app.py
@@ -9,18 +9,79 @@ from model.user_reviews import UserReview
 from model.place_images import PlaceImage
 from model.place_reviews import PlaceReview
 
+# Basic setup
 if not connection.testConnection():
     print("Connection to the DB was not possible. Exiting program...")
     sys.exit()
-
 model.createTables()
-
 app = Flask(__name__)
 session = connection.getConnection()
 
+# Endpoints
 @app.route('/')
 def helloWorld():
     return jsonify('Hello'), 200
 
+## Users
+@app.route('/users/get')
+def get_user():
+    try:
+        id = request.args.get('id')
+
+        # Exception handling
+        ## Check id argument is not null
+        if id is None:
+            raise Exception('Id cannot be null')
+        ## Check id argument is not empty
+        if len(id) == 0:
+            raise Exception('Id cannot be empty')
+        ## Check id is an int - it throws ValueError if not 
+        int(id)
+
+        # Make query to database
+        user = session.query(User).filter(User.id == id).first()
+
+        # Check an user exists with the id
+        if user is None:
+            raise Exception('No user exists with this id')
+        
+        # Format result and send it
+        result = getUserDict(user)
+        return jsonify(result), 200
+        
+    except ValueError as e:
+        return jsonify({'error': 'Id has to be an integer'}), 400
+    except Exception as e:
+        code = None
+        if str(e) == 'No user exists with this id':
+            code = 404
+        else:
+            code = 400
+        return jsonify({'error': str(e)}), code
+
+@app.route('/users/getAll')
+def get_all_users():
+    users = session.query(User)
+    
+    result = []
+    for row in users:
+        user = getUserDict(row)
+        result.append(user)
+    
+    return jsonify(result), 200
+
+def getUserDict(data):
+    return {
+        "id": data.id,
+        "name": data.name,
+        "lastName": data.lastName,
+        "age": data.age,
+        "nationality": data.nationality,
+        "description": data.description,
+        "profile_image": data.profile_image,
+        "stars": data.stars
+    }
+
+# Run app
 app.run(debug=True)
 connection.closeConnection()
