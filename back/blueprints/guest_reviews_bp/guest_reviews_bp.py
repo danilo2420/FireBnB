b263615b1ac7e03231b83fd45aec39343ab4e0e2
danilo2420 <danimoraga1997@gmail.com>
2025-05-06 15:19:29 +0200

    guest reviews get endpoints done

--- back/blueprints/guest_reviews_bp/guest_reviews_bp.py
+++ back/blueprints/guest_reviews_bp/guest_reviews_bp.py
@@ -3,6 +3,8 @@ from blueprints.guest_reviews_bp.schemas.guest_reviews_input_schemas import *
 from blueprints.guest_reviews_bp.schemas.guest_reviews_output_schemas import *
 from connection import getConnection
 from model.guest_reviews import GuestReview
+from model.schemas.guestReviewSchema import GuestReviewSchema
+from model.users import User
 
 bp = Blueprint('guest_reviews_bp', __name__, url_prefix='/guest_reviews')
 
@@ -16,12 +18,47 @@ def get_all_guest_reviews():
 
 
 @bp.route('/get')
-def get_guest_review():
-    ...
+@bp.arguments(Id_InputSchema, location='query')
+@bp.response(200, GuestReviewSchema)
+def get_guest_review(args):
+    id = args.get('id')
+    session = getConnection()
 
-## Get all guest's reviews
+    guest_review = session.query(GuestReview).filter(GuestReview.id == id).first()
+    if guest_review is None:
+        abort(404, message='no guest review was found with this id')
+    
+    return guest_review 
 
 ## Get all host's reviews
+@bp.route('/get_all_for_host')
+@bp.arguments(Id_InputSchema, location='query')
+@bp.response(200, GetAll_OutputSchema)
+def get_guest_reviews_for_host(args):
+    id = args.get('id')
+    session = getConnection()
+
+    # Check host exists
+    host = session.query(User).filter(User.id == id).first()
+    if host is None:
+        abort(404, message='host user was not found with this id')
+    
+    return {'guest_reviews': host.host_reviews}
+
+## Get all guest's reviews
+@bp.route('/get_all_for_guest')
+@bp.arguments(Id_InputSchema, location='query')
+@bp.response(200, GetAll_OutputSchema)
+def get_guest_reviews_for_guest(args):
+    id = args.get('id')
+    session = getConnection()
+
+    # Check host exists
+    guest = session.query(User).filter(User.id == id).first()
+    if guest is None:
+        abort(404, message='guest user was not found with this id')
+    
+    return {'guest_reviews': guest.guest_reviews}
 
 # C
 
