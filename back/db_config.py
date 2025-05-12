c46392f2ff8d32d0fbaa174eb14d30ab5e46bfac
danilo2420 <danimoraga1997@gmail.com>
2025-05-12 19:14:06 +0200

    abcd

--- back/db_config.py
+++ back/db_config.py
@@ -1,8 +1,8 @@
 # Data to access database
 DB_USER = 'root'
-# Work: 
-DB_PASSWORD = '1a2b3c4d5e!$'
-# Home: DB_PASSWORD = 'root'
+# Work: DB_PASSWORD = '1a2b3c4d5e!$'
+# Home: 
+DB_PASSWORD = 'root'
 DB_PORT = '5432'
-DB_NAME = 'FireBnb'
-# DB_NAME = 'FireBnB'
+# DB_NAME = 'FireBnb'
+DB_NAME = 'FireBnB'
